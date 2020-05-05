99d5cb71e52c713e998cea02aab1b9d10060592b
worldwidekatie <katie.evankodouglas@gmail.com>
2020-05-05 14:30:33 -0600

    Made changes

---
+++ my_lambdata/my_mod.py
@@ -0,0 +1,19 @@
+
+
+def enlarge(n):
+    return int(n)*100
+
+
+if __name__ == "__main__":
+    pass
+    # only run the following code
+    # when we run this script rom the command-line
+    # otherwise don't invoke this code 
+    # (for example when importing from another file)
+
+    x = 5
+    print(enlarge(x))
+
+
+    z = input("Please choose a number to enlarge:")
+    print(enlarge(z))
